e8220cfdd23861f1bd2939c83ecb3bf3ec7533bc
YKPandit <yaaskakpandit@gmail.com>
2025-04-23 14:12:18 -0700

    reworked the code, starting with data collection

--- model.py
+++ model.py
@@ -12,19 +12,26 @@ from torch.utils.data import DataLoader
 # Processing imports
 import pandas
 import numpy
-from sklearn.preprocessing import StandardScalar
+from sklearn.preprocessing import StandardScaler
 
 # Local Model Definition
 from SimpleNN import SimpleNN
+
+# Other
+import os
 
 # Functions
 
 """ 
 Name: getQualiInfo
-Param: Race:str
+Param: Race:str year:int
 Return: dataframe of the session's information
 Description: Will take in a race name and year then get the qualifying session for that year
 """
+def getQualiInfo(race:str, year:int):
+    session = None
+    return session
+
 
 """
 Name: mergeSessions
@@ -47,19 +54,39 @@ Description:
         7. Train model with data
         8. Save the trained model
 """
-
+def train_mode(race:str):
+    print("Starting Data Collection")
+    year_1 = getQualiInfo(race, 2022)
+    year_2 = getQualiInfo(race, 2023)
+    year_3 = getQualiInfo(race, 2024)
+    print("Finished Data Collection")
 
 
 ### MAIN ###
 
 # Get all the races of the season
+season = fastf1.get_event_schedule(2025)
 
 # Create a directory to store all sessions
+directory_name = "Models"
+
+# Create the directory
+try:
+    os.mkdir(directory_name)
+    print(f"Directory '{directory_name}' created successfully.")
+except FileExistsError:
+    print(f"Directory '{directory_name}' already exists.")
+except PermissionError:
+    print(f"Permission denied: Unable to create '{directory_name}'.")
+except Exception as e:
+    print(f"An error occurred: {e}")
+
 
 # Loop through all the seasons
-
+for race in season['Location']:
     # Print the race
-
+    print(race)
     # Train a model -> make sure it is saves
+    train_model(race)
 
 # Take in Race, Driver, Condition, Previous Lap, Tire Age and give a prediction
